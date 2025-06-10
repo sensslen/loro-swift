bee00a27743c31c9f06da500af3709d60283a01c
Simon Ensslen <sensslen@gmail.com>
2025-06-10 21:15:56 +0200

    fix

--- scripts/build_macos.sh
+++ scripts/build_macos.sh
@@ -7,7 +7,7 @@
 set -euxo pipefail
 THIS_SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 LIB_NAME="libloro_ffi.a"
-RUST_FOLDER="$THIS_SCRIPT_DIR/../loro-ffi"
+RUST_FOLDER="$THIS_SCRIPT_DIR/../loro-swift"
 FRAMEWORK_NAME="loroFFI"
 
 SWIFT_FOLDER="$THIS_SCRIPT_DIR/../gen-swift"
